79f562628641539e16a55781304ab2ddf27cffe4
Laurin Quast <laurinquast@googlemail.com>
2023-05-06 20:01:32 +0200

    chore: use yoga instead of ez for the example app (#199)

--- example/src/server.ts
+++ example/src/server.ts
@@ -1,24 +1,10 @@
-import Koa from "koa";
-import KoaRouter from "@koa/router";
+import { yoga } from "./yoga";
+import { createServer } from "http";
 
-import { ezApp } from "./app";
+const port = 8080;
 
-const app = new Koa();
-
-const router = new KoaRouter();
-
-ezApp
-  .buildApp({
-    app,
-    router,
-  })
-  .then(() => {
-    app.use(router.routes()).use(router.allowedMethods());
-
-    const port = 8080;
-    app.listen(port, () => {
-      console.log(
-        `GraphQL Server listening on port http://127.0.0.1:${port}/graphql`
-      );
-    });
-  });
+createServer(yoga).listen(port, () => {
+  console.log(
+    `GraphQL Server listening on port http://127.0.0.1:${port}/graphql`
+  );
+});
